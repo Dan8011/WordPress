7dd56cb32005a422575cece236fae2e9e70f8e02
Daniel Koukolík <Daniel.Koukolik@student.souepl.cz>
2025-10-09 11:29:04 +0200

    vlny

--- wp-content/themes/Moje_šablona/functions.php
+++ wp-content/themes/Moje_šablona/functions.php
@@ -1,29 +1,7 @@
 <?php
-// Vytvoření globální proměnné s URL šablony
-function moje_globalni_promenne() {
-    // Používá funkci get_template_directory_uri() pro získání URL šablony
-    $GLOBALS['moje_sablona_url'] = get_template_directory_uri();
+function vlastni_sablona_enqueue_styles() {
+    // Registrace a načtení hlavního stylu
+    wp_register_style('vlastni-style', get_template_directory_uri() . '/style.css', array(), filemtime(get_template_directory() . '/style.css'), 'all');
+    wp_enqueue_style('vlastni-style');
 }
-add_action('init', 'moje_globalni_promenne');
- 
-function moje_sablona_nacti_scripty() {
-    // CSS
-    wp_enqueue_style(
-        'moje-styly',
-        get_template_directory_uri() . '/style.css',
-        array(),
-        filemtime(get_template_directory() . '/style.css')
-    );
-
-    // JavaScript
-    wp_enqueue_script(
-        'moje-skript',
-        get_template_directory_uri() . '/script/script.js',
-        array(), // zde můžeš přidat závislosti (např. jQuery)
-        filemtime(get_template_directory() . '/script/script.js'),
-        true // true = vloží do footeru
-    );
-}
-add_action('wp_enqueue_scripts', 'moje_sablona_nacti_scripty');
- 
-?>
+add_action('wp_enqueue_scripts', 'vlastni_sablona_enqueue_styles');
